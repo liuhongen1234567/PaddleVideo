6718fd54dd89292cf606e2d5e881c528ff6ea3fd
liuhongen1234567 <2998388548@qq.com>
2024-11-21 02:08:36 +0000

    modify download.py

--- paddlevideo/tasks/download.py
+++ paddlevideo/tasks/download.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#   Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 import os
 import sys
